e2b499be15f139b4eed42153762182c75442b644
Sonia Kandah <sonia.kandah@gmail.com>
2019-09-27 22:45:23 -0700

    Comments and clarifying edits to demo

--- curriculum/class-01/demo/modules-single-export/index.js
+++ curriculum/class-01/demo/modules-single-export/index.js
@@ -1,12 +1,15 @@
-'use strict';
+"use strict";
 
-// Pull in our 'hello' module
-const hello = require('./lib/hello.js');
+// Here we import a module located in the
+// file ./lib/hello.js, and we name it "hello"
+const hello = require("./lib/hello.js");
 
-// What is 'hello?'  As you'll see from this console.log, it's a function that you can call.
+// Let's try to print out what the variable
+// "hello" actually stored
 console.log(hello);
 
-// Call that function directly, and it should log out 'Hello, John'
-hello('john');
-
-// Here, we imported a node module that itself is just a function, and then ran it.
+// Now lets try to use "hello" as if it was
+// a function. That's because if you look
+// in ./lib/hello.js you'll see that it is a
+// function
+hello("john");
--- curriculum/class-01/demo/modules-single-export/lib/hello.js
+++ curriculum/class-01/demo/modules-single-export/lib/hello.js
@@ -1,11 +1,10 @@
-'use strict';
+"use strict";
 
-// module.exports represents our 'interface' to the outside world.
-// Anything that we attach to the exports object is therefore "exposed" or "exported" publicly
-// In this case, we're exposing just a raw function, so that when we are "required" in, the user can just run this as a method.
-module.exports = exports = (name) => {
-  console.log( `Hello ${name}` );
+// We use module.exports to define this
+// function as a CommonJS module that we're
+// exposing to whoever wants to use add this
+// file to their project
+
+module.exports = exports = name => {
+	console.log(`Hello ${name}`);
 };
-
-
-
