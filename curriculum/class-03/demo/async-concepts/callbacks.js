d321e6c6cbd7cbf3ad013150ac515d3e4a284d4a
Sonia Kandah <sonia.kandah@gmail.com>
2019-10-05 00:27:10 -0700

    Update demos

--- curriculum/class-03/demo/async-concepts/callbacks.js
+++ curriculum/class-03/demo/async-concepts/callbacks.js
@@ -1,65 +1,22 @@
-'use strict';
+"use strict";
 
 // Callbacks OFFLOAD functionality.
-// Think of it as though you're telling a method to go do it's work and when it's done, to handle it for you
-// A good analogy would be to picture hiring a moving company to move you
-//   You give them all of your stuff AND put workers in the back of the truck with instructions
-//   When they get there, they read the instructions and move you in.
-//   You don't have to worry about when, and you already told them how.  At some point, it'll just get done
-//   according to your specifications.
+// Start an async process, and send it a callback function
+// When the async process finishes, run the callback function
 
-let myCallback = (data) => {
-  console.log('2: Received', data);
-};
+// We'll simulate an async function that calls an api
+// Behind the scenes it's just setTimeout
 
-let useTheCallback = (cb) => {
-  let text = 'some random text';
-  console.log('1: Calling the callback');
-  cb(text);
-  console.log('3: After the callback');
-};
+// We use a random number for setTimeout to simulate that
+// the response time for an api can be variable
 
-useTheCallback(myCallback);
+// Multiple async events in a row
+// Because they are async, they may not necessarily
+// run in order.
 
-// ERROR FIRST Callbacks --- always expect to be called with the first parameter being either an
-// error when an error occurs, or as undefined when not.  In the "good" case, data is given as
-// the second parameter.
+// When we pass a callback to our apiCall, we
+// can enforce things to be run in a certain order,
+// even though they're async!
 
-let errorFirstCallback = (err, data) => {
-  if ( err ) { throw err; }
-  console.log('2: Received', data);
-};
-
-let useTheErrorFirstCallback = (cb) => {
-  // do lots of work and come up with some text
-  let text = 'some random text';
-  console.log('1: Calling the error first callback');
-  cb(undefined, text);
-  console.log('3: After the error first callback');
-};
-
-
-useTheErrorFirstCallback(errorFirstCallback);
-
-
-// Asynchronous Callbacks -- you have no guarantee of run order!
-// The setTimeout here simulates this taking a long time to run
-// It goes into the callback queue, not the call stack (until it finishes)
-let asyncCallback = (err, data) => {
-  if ( err ) { throw err; }
-  console.log('2: Received', data);
-};
-
-let useTheAsyncCallback = (cb) => {
-  // do lots of work and come up with some text
-  let text = 'some random text';
-  console.log('1: Calling the async callback');
-  setTimeout( () => {
-    cb(undefined, text);
-  },1000);
-  console.log('3: After the async callback');
-};
-
-
-useTheAsyncCallback(asyncCallback);
-
+// The above code works, but it's not really pretty!
+// Promises help a little
--- curriculum/class-03/demo/async-concepts/promises.js
+++ curriculum/class-03/demo/async-concepts/promises.js
@@ -1,41 +1,23 @@
-'use strict';
+"use strict";
 
 // Promises DECLARE functionality
-// Think of it as though you're telling a method to go do it's work and when it's done, give you the data
+// A Promise is a class! It holds the eventual
+// result of an async operation. When the async
+// operation completes, it can either result in
+// a value or an error. A promise holds three
+// states: pending, fulfilled/resolved or rejected
 
-let longTask = (foo) => new Promise( (resolve,reject) => {
-  if( foo ) { resolve('1'); }
-  else { reject('Bad'); }
-});
+// We use a random number for setTimeout to simulate that
+// the response time for an api can be variable
 
-longTask(true)
-  .then(task => console.log('Task', task))
-  .catch(console.error);
+// We return a Promise object, which has a
+// function given to it as a parameter. This
+// function contains the async code to run,
+// as well as determining when to resolve or
+// reject the promise.
 
+// Now let's try to do these in order, even
+// though they are async:
 
-// Running many async things in a series
-longTask(true)
-  .then( data => { console.log(data); return longTask(data); } )
-  .then( data => { console.log(data); return longTask(data); } )
-  .then( data => { console.log(data); return longTask(data); } )
-  .then( data => { console.log(data); return longTask(false); } )
-  .then( data => { console.log(data); return longTask(data); } )
-  .then( data => { console.log(data); return longTask(data); } )
-  .then( data => { console.log(data); return longTask(data); } )
-  .then( data => { console.log(data); return longTask(data); } )
-  .catch(err => { console.error('catch', err); return true; } )
-  .then( data => { console.log(data); return longTask(data); } )
-  .then( data => { console.log(data); return longTask(false); } )
-  .then( data => { console.log(data); return longTask(data); } )
-  .catch(err => console.error('quit', err))
-
-// Running Many Async Things Simultaneously ...
-let stuffToDo = [];
-for( let i = 1; i <= 10; i++ ) {
-  stuffToDo.push(longTask(true));
-}
-Promise.all(stuffToDo)
-  .then(things => console.log('Things', things))
-  .catch(console.error);
-
-
+// By chaining .then and .catch, we can create a
+// synchronous way to do async!
