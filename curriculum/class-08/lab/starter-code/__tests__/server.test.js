fb8aa52f924225a0ae67bfb9ca707d39a4c0e85b
Sonia Kandah <sonia.kandah@gmail.com>
2019-10-22 16:58:30 -0700

    Update server.test.js

--- curriculum/class-08/lab/starter-code/__tests__/server.test.js
+++ curriculum/class-08/lab/starter-code/__tests__/server.test.js
@@ -18,26 +18,4 @@ describe('web server', () => {
       })
       .catch(console.error);
   });
-
-  test('async/await: should respond properly on request to /people', async () => {
-    try {
-      let res = await mockRequest.get('/people');
-
-      expect(res.status).toBe(200);
-      expect(res.body.count).toBe(4);
-    } catch (e) {
-      console.error(e);
-    }
-  });
-
-  it('should respond properly on post to /people', () => {
-    mockRequest
-      .post('/people')
-      .send({ firstName: 'Test', lastName: 'Person' })
-      .then(results => {
-        expect(results.status).toBe(200);
-        expect(results.body.firstName).toBe('Test');
-      })
-      .catch(console.error);
-  });
 });
