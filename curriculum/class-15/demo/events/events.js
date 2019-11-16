f8073707e1aae63e92e4558d9414363e5c496d19
Sonia Kandah <sonia.kandah@gmail.com>
2019-11-15 19:33:47 -0800

    update demos

---
+++ curriculum/class-15/demo/events/events.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// Let's work with some events!
+// on, off, once, emit, listeners
--- curriculum/class-15/demo/events/modular/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const events = require('./events.js');
-
-const logger = require('./logger.js');
-const cache = require('./cache.js');
-
-events.emit('save', {id:77});
